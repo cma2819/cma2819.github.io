6fa4b4ca3b25eda92a2f3d9befe5f53afbbdc070
cma2819 <cma2819@users.noreply.github.com>
2021-11-03 14:02:42 +0000

    deploy: b191b89d70b8785ff88a12ab4a2c24d9016ac876

--- _next/static/OWRVLl6pxYJrQmSw59paF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3ee4778cba5051721c4a.css","static/chunks/pages/index-07699bf7eb6008a498de.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
